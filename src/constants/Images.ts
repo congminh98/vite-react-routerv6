89dc0b0b769eb4f32c470c5ed0569a83ddfc67d1
Minh Phan C. VN.Danang <minh.phan2@monstar-lab.com>
2023-01-09 23:27:50 +0700

    .

--- src/constants/Images.ts
+++ src/constants/Images.ts
@@ -5,7 +5,6 @@ import THUMBNAIL from "@/assets/images/anhcuoi.jpeg";
 import FB_SVG from "@/assets/images/facebook.svg";
 import INSTAGRAM_SVG from "@/assets/images/instagram.svg";
 import BRIDE from "@/assets/images/codau.jpeg";
-import BRIDECIRCLE from "@/assets/images/codau-circle.png";
 import DIVIDER from "@/assets/images/divider.png";
 import WEDDING_LOGO from "@/assets/images/wedding.png";
 
@@ -17,7 +16,6 @@ const IMAGES = {
   FB_SVG: FB_SVG,
   INSTAGRAM_SVG: INSTAGRAM_SVG,
   BRIDE: BRIDE,
-  BRIDECIRCLE: BRIDECIRCLE,
   DIVIDER: DIVIDER,
   WEDDING_LOGO: WEDDING_LOGO,
 };
